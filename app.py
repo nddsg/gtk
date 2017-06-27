4d1d4bea7633bd63ef9b5aa58045e8472d4e5caa
Samuel H Cho <scho2@dsg1.virtual.crc.nd.edu>
2017-06-27 09:14:40 -0400

    6/27/17 Work in Progress... Started process of adding data to database and implementing checks to make sure data is available from user to send.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, send_from_directory
 from random import randint
 from json import dumps
+from uuid import uuid4
+from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -22,7 +24,6 @@ def js():
 def send_img(path):
     return send_from_directory('images', path)
 
-
 class API():
     def __init__(self, session_maker):
         self.session_maker = session_maker
@@ -36,11 +37,94 @@ class API():
         
         return output
 
+    # A simple method to support multiple levels of dictionary indirection
+    @staticmethod
+    def get_i(obj, keys, default=None)
+        while len(keys) > 0:
+            indirect_key = keys.pop(0)
+            obj = obj.get(indirect_key, default)
+            if obj is default:
+                return default
+        return obj
+            
+
     def random_pair(self):
+        output = {}
         session = self.session_maker()
+        
+        post_parameters = request.get_json()
+        
+        # Check if the user_id is set. If not, we need to give them one
+        user_id, user_id_was_set = post_paramters.get("user_id", None), True
+        if user_id is None:
+            user_id_was_set = False
+            # Generate a new user_id to send to the user
+            user_id = uuid4()
+            session.add(User(userid=user_id))
+            output.update({"user_id": user_id})
+        else:
+            # Check if the quiz_id is set. If not, we need to give them a new one
+            quiz_id, quiz_id_was_set = post_parameters.get("quiz_id", None), True
+            if quiz_id is None:
+                quiz_id_was_set = False
+                quiz_id = uuid4()
+                session.add(InitialQuiz(quizid=quiz_id, userid=user_id))
+            else:
+                # Check if the round_id is set. If not, we need to create a new round_id
+                round_id, round_id_was_set = post_parameters.get("round_id", None), True
+                if round_id is None:
+                    round_id_was_set = False
+                    round_id = uuid4()
+                else:
+                    # Check if the round_no is set. If not, we need to create a new round_id because the round is invalid
+                    round_no, round_no_was_set = post_parameters.get("round_no", None), True
+                    if round_no is None:
+                        round_id_was_set, round_no_was_set = False, False
+                        round_id = uuid4()
+                        round_no = 0
+                    else:
+                        # We need to create a new round since round no was not set (round was invalid)
+                        round_id_was_set, round_no_was_set = False, False
+                        round_id = uuid4()
+                        round_no = 0
+        
+        ############################################################################################
+        ## SANITY CHECK: after this barrier, each of the following should be defined:              #
+        ## user_id                                                                                 #
+        ## quiz_id                                                                                 #
+        ## round_id                                                                                #
+        ## round_no                                                                                #
+        ## We can comment this out once we are reasonably sure the logic above has no bugs.        #
+        ############################################################################################
+        try:
+            user_id, quiz_id, round_id, round_no
+        except NameError as e:
+            print("Logic Error (required variable unset): " + e)
+        ############################################################################################
+        ## END SANITY CHECK                                                                        #
+        ############################################################################################
 
+        if all([quiz_id_was_set, round_id_was_set, round_no_was_set, user_id_was_set]):
+            # Check if the user sent output for us to record from a previous round
+            preferred_choice = self.get_i(post_parameters, ["preferredChoice", "value"])
+            preferred_choice_time = post_parameters.get("preferredChoiceTime")
+            upvote_choice = self.get_id(post_parameters, ["upvoteChoice", "value"])
+            upvote_choice_time = post_parameters.get("upvoteChoiceTime")
+            if all(i is not None for i in [preferred_choice, preferred_choice_time, upvote_choice, upvote_choice_time]):
+                # All the necessary data is set, so we can record the information
+                
+                # TODO:
+                #  - ADD ALL THE DATA TO THE ROUND DATABASE OBJECT
+                #  - DELETE THE TEMPORARY INITIAL ROUND OBJECT
+                
+                # This round was successfully recorded, so we can serve the next round
+                round_no += 1
+        
+        # BEGIN GENERATE NEW ROUND STUFF
+        
+        # Choose two random images
+        # TODO: make this more efficient; choose two random images from the same subreddit
         result = session.query(Image)
-
         count = result.count()
         if count < 2:
             raise LookupError("Not enough information error")
@@ -51,7 +135,16 @@ class API():
 
         results = result.all()
 
-        output = dumps({
+#         start_time = datetime()
+#         session.add(InitialRound(
+#             roundid=round_id,
+#             roundno=round_no
+#             starttime=start_time,
+#             image1id=results[idx1].id,
+#             image2id=results[idx2].id,
+#         ))
+
+        output.update({
             "image1": {
                 "image_id": results[idx1].id,
 	            "subreddit": results[idx1].subreddit,
@@ -67,8 +160,9 @@ class API():
             	"url": results[idx2].image
             }
         })
-
-        return output
+        
+        session.commit()
+        return dumps(output)
 
 
 if __name__ == '__main__':
--- dataModel.py
+++ dataModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Time
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Time, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
@@ -7,23 +7,66 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "user"
-    userid = Column(Integer, nullable=False, primary_key=True)
+    userid = Column(String(40), nullable=False, primary_key=True)
+    fingerprint_canvas_hash = Column(String(32))
+    fingerprint_screen_size_x = Column(Integer)
+    fingerprint_screen_size_y= Column(Integer)
+    fingerprint_screen_color_depth= Column(Integer)
+    fingerprint_timezone = Column(Integer)
+    fingerprint_dnt_enabled = Column(Boolean)
+    fingerprint_http_accept_headers = Column(String(512))
+    fingerprint_webgl_hash = Column(String(32))
+    fingerprint_language = Column(String(16))
+    fingerprint_platform = Column(String(32))
+    fingerprint_useragent = Column(String(255))
+    fingerprint_user_ip = Column(String(15))
+
+class UserPlugins(Base):
+    __tablename__ = "fingerprint_browser_plugins"
+    userid = Column(String(40), ForeignKey("user.userid"), primary_key=True)
+    plugin_string = Column(String(255), primary_key=True)
+
+
+class UserFonts(Base):
+    __tablename__ = "fingerprint_system_fonts"
+    userid = Column(String(40), ForeignKey("user.userid"), primary_key=True)
+    font_string = Column(String(128), primary_key=True)
+
+
+class InitialQuiz(Base):
+    __tablename__ = "initial_quiz"
+    quizid = Column(String(40), primary_key=True)
+    userid = Column(String(40), ForeignKey("user.userid"))
 
 
 class Quiz(Base):
     __tablename__ = "quiz"
-    quizid = Column(Integer, primary_key=True)
-    userid = Column(Integer, ForeignKey("user.userid"))
+    quizid = Column(String(40), primary_key=True)
+    userid = Column(String(40), ForeignKey("user.userid"))
 
+
+class InitialRound(Base):
+    __tablename__ = "initial_round"
+    roundid = Column(String(40), primary_key=True)
+    roundno = Column(Integer, nullable=False)
+    starttime = Column(DateTime())
+    image1id = Column(Integer, ForeignKey("image.id"))
+    image2id = Column(Integer, ForeignKey("image.id"))
+    quizid = Column(String(40), ForeignKey("initial_quiz.quizid"))
+    
 
 class Round(Base):
     __tablename__ = "round"
-    roundid = Column(Integer, primary_key=True)
-    quizid = Column(Integer, ForeignKey("quiz.quizid"))
+    roundid = Column(String(40), primary_key=True)
+    quizid = Column(String(40), ForeignKey("quiz.quizid"))
     image1id = Column(Integer, ForeignKey("image.id"))
     image2id = Column(Integer, ForeignKey("image.id"))
-    time = Column(Time())
-    outcome = Column(String(32))
+    starttime = Column(DateTime())
+    preferredchoicetime = Column(Time())
+    upvotechoicetime = Column(Time())
+    preferredchioce = Column(Integer, ForeignKey("image.id"))
+    upvotechoice = Column(Integer, ForeignKey("image.id"))
+    correct = Column(Boolean)
 
 
 class Image(Base):
